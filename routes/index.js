21f73c4eee819ecb65b67a5b58ec80245529c16f
subJM <thswhdals@naver.com>
2024-04-02 14:48:15 +0900

    saveLog 저장모듈

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+// saveLog 함수 가져오기
+var saveLog = require('../utils/logger');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/test', function(req, res, next){
+  saveLog('check');
+  res.send('test');
+});
+
+router.post('/gitApiSaveLog', function(req, res, next){
+  var postData = req.body;
+  saveLog(postData);
+});
+
+
 
 module.exports = router;
