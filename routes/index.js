ee1c6685abfaadad6830bffeadef3892ad47dc04
thswhdals <thswhdals@banco.id>
2024-04-04 14:29:38 +0900

    test

--- routes/index.js
+++ routes/index.js
@@ -4,47 +4,57 @@ var router = express.Router();
 const axios = require('axios');
 
 // saveLog 함수 가져오기
-var saveLog = require('../utils/logger');
+const saveLog = require('../utils/logger');
+
+const chatId = '2131291509';
+const telegramToken = '7148869581:AAESpzk-Gk-vBAyKgfr9aiyT5Nk13a21jdA';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 router.get('/test', function(req, res, next) {
-  saveLog('네트웍 테스트용');
-});
 
-router.post('/gitApiSaveLog', function(req, res, next){
-  var postData = req.body;
-  // 요청 본문을 문자열로 변환하여 로그에 저장
-  var logMessage = JSON.stringify(postData);
-  saveLog('<======================= gitApiSaveLog ==========================>');
-  saveLog(logMessage.repository.head_commit);
-  saveLog('전체 데이터');
-  saveLog(logMessage);
-
-  console.log(logMessage);
-
-  const data = logMessage.repository.head_commit;
-
-  const message = {
-    '작성자' : data.author.name,
-    '작성자 이메일' : data.author.email,
-    '배포자' : data.committer.name,
-    '배포자 이메일' : data.committer.email,
-    '배포시간' : data.timestamp
-  }
-
-  const chatId = '29833358';
-  const telegramToken = '7148869581:AAESpzk-Gk-vBAyKgfr9aiyT5Nk13a21jdA';
-
-  const sendMessageUrl = `https://api.telegram.org/bot${telegramToken}/sendMessage?chat_id=${chatId}&text=${encodeURIComponent(message)}`;
+  const sendMessageUrl = `https://api.telegram.org/bot7148869581:AAESpzk-Gk-vBAyKgfr9aiyT5Nk13a21jdA/getUpdates`;
 
   var result = axios.get(sendMessageUrl);
   console.log('result');
   console.log(result);
-  // 클라이언트에게 요청 본문 그대로 응답
-  res.send(logMessage);
+});
+
+router.get('/gitApiSaveLog', function(req, res, next){
+  var postData = req.body;
+  // 요청 본문을 문자열로 변환하여 로그에 저장
+  var logMessage = JSON.stringify(postData);
+  saveLog('<======================= gitApiSaveLog ==========================>');
+  saveLog(JSON.(logMessage.head_commit));
+  saveLog('전체 데이터');
+  saveLog(logMessage);
+
+  const data = logMessage.head_commit;
+  const message = `작성자: ${data.author.name}\n` +
+                  `작성자 이메일: ${data.author.email}\n` +
+                  `배포자: ${data.committer.name}\n` +
+                  `배포자 이메일: ${data.committer.email}\n` +
+                  `배포시간: ${data.timestamp}`;
+
+  console.log((message));
+
+  axios.post(`https://api.telegram.org/bot${telegramToken}/sendMessage`, { 
+    chat_id: chatId,
+    text: message,
+  })
+  .then(response => {
+    console.log('메시지가 성공적으로 전송되었습니다:', response.data);
+    res.send(response.data);
+  })
+  .catch(error => {
+    console.error('메시지 전송에 실패하였습니다:', error);
+    res.send(error);
+  });
+
+  // // 클라이언트에게 요청 본문 그대로 응답
+  // res.send(logMessage);
 });
 
 
