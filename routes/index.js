4e28b62b1d9830885293ca3603a960a94d1cdf13
thswhdals <thswhdals@banco.id>
2024-04-04 15:14:22 +0900

    한국시간으로 변경

--- routes/index.js
+++ routes/index.js
@@ -27,17 +27,26 @@ router.post('/gitApiSaveLog', function(req, res, next){
   saveLog(logMessage);
 
   const data = postData.head_commit;
-  const timestamp = new Date(data.timestamp);
+  const timestamp = new Date(data.timestamp,);
   const message = `작성자: ${data.author.name}\n` +
                   `작성자 이메일: ${data.author.email}\n` +
                   `배포자: ${data.committer.name}\n` +
                   `배포자 이메일: ${data.committer.email}\n` +
-                  `배포시간: ${timestamp}`;
+                  `배포시간: ${formatDate(timestamp)}`;
 
   sendMassage(message);
 
 });
 
+const formatDate = (date) => {
+  const year = date.getFullYear();
+  const month = ('0' + (date.getMonth() + 1)).slice(-2);
+  const day = ('0' + date.getDate()).slice(-2);
+  const hours = ('0' + date.getHours()).slice(-2);
+  const minutes = ('0' + date.getMinutes()).slice(-2);
+  const seconds = ('0' + date.getSeconds()).slice(-2);
 
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+};
 
 module.exports = router;
