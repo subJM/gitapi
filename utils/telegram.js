21c5c86556191d2a3bc350fc00b48bd5dbf9a966
thswhdals <thswhdals@banco.id>
2024-04-04 15:18:35 +0900

    메시지 로그 수정

--- utils/telegram.js
+++ utils/telegram.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const saveLog = require('./logger');
 
 const chatId = '2131291509';
 const telegramToken = '7148869581:AAESpzk-Gk-vBAyKgfr9aiyT5Nk13a21jdA';
@@ -10,12 +11,10 @@ function sendMassage(message) {
     text: message,
   })
   .then(response => {
-    console.log('메시지가 성공적으로 전송되었습니다:', response.data);
-    res.send(response.data);
+    saveLog('메시지가 성공적으로 전송되었습니다 : '+ JSON.stringify(response.data));
   })
   .catch(error => {
-    console.error('메시지 전송에 실패하였습니다:', error);
-    res.send(error);
+    saveLog('메시지 전송에 실패하였습니다 : '+ JSON.stringify(error));
   });
 }
 
