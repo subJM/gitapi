0d77e156fc66047e6a2a9f2bf0701eecd610a296
jongmin87 <thswhdals@banco.id>
2024-04-17 10:25:00 +0900

    로그 한국시간대로 변경

--- utils/logger.js
+++ utils/logger.js
@@ -1,21 +1,18 @@
-
 const fs = require('fs');
 const path = require('path');
-// 로그 메시지를 저장하는 함수
-function saveLog(message) {
-  // 현재 날짜를 YYYY-MM-DD 형식으로 포맷
-  const date = new Date();
-  const formattedDate = date.toISOString().split('T')[0];
+const moment = require('moment-timezone');
 
-  // 로그 파일 경로 생성 (예: logs/2023-03-28.log)
+function saveLog(message) {
+  const date = moment().tz('Asia/Seoul');
+  const formattedDate = date.format('YYYY-MM-DD');  //파일이름
+
   const logFileName = `${formattedDate}.log`;
   const logFilePath = path.join(__dirname, '../logs', logFileName);
 
-  // 로그 메시지에 타임스탬프 추가
-  const timestamp = new Date().toISOString();
-  const logMessage = `[${timestamp}] ${message}\n`;
+  const timestamp = date.format(); // 로그 찍히는 시간
 
-  // 파일에 로그 메시지 추가 (파일이 없으면 생성)
+  const logMessage = `[${timestamp}] ${message}\n`; //메세지
+
   fs.appendFile(logFilePath, logMessage, (err) => {
     if (err) {
       console.error('로그를 파일에 저장하는데 실패했습니다:', err);
